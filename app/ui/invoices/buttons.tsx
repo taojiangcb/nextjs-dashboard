5b444ca2e0cd4540750ff8fafaecc0703ab1a5c7
taojiang <jiangtao_qepyou@163.com>
2024-12-24 20:24:04 +0800

    fix(app) drop some code for the lint

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,5 +1,4 @@
-import { deleteInvoice } from '@/app/lib/actions';
-import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
+import { PencilIcon, PlusIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
 export function CreateInvoice() {
